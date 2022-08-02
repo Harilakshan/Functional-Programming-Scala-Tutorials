f20110bcbcb413abc7cdaa33e1ab58417e7909af
Harilakshan <harirharilakshan@gmail.com>
2022-08-02 09:45:09 +0530

    Delete Qus_3.scala

--- Qus_3.scala
+++
@@ -1,9 +0,0 @@
-package tutorial_3
-
-object Qus_3 extends App {
-  def CalcVolume(radius: Double): Double = (4.0d / 3.0d) * scala.math.Pi * radius * radius
-
-  val radius = 5.0d
-  println(s"The volume of sphere with radius $radius is ${CalcVolume(radius)}")
-}
-
