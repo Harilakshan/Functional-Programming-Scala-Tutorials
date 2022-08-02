9702fe7b142594a2a199aeee1266f20b8b5c7301
Harilakshan <harirharilakshan@gmail.com>
2022-08-02 09:45:50 +0530

    Delete Qus_5.scala

--- Qus_5.scala
+++
@@ -1,9 +0,0 @@
-package tutorial_3
-
-object Qus_5 extends App {
-  def GetEasytime(distance: Double): Double = 8 * distance
-
-  def GetTempotime(distance: Double): Double = 7 * distance
-
-  println(s"Total running time is ${GetEasytime(2) + GetTempotime(3) + GetEasytime(2)}")
-}
